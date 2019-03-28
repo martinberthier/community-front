7646fd8239e55ee3b8ffeb8ae8f30bd202090d4e
martinberthier <berthier.martin@gmail.com>
2019-03-28 16:53:00 +0100

    categoryId en paramUrl de postAdd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     component: PostListComponent
   },
   {
-    path: 'post-add',
+    path: 'post-add/:idCategory',
     component: PostEditComponent
   },
   {
--- src/app/post-list-by-category/post-list-by-category.component.ts
+++ src/app/post-list-by-category/post-list-by-category.component.ts
@@ -14,6 +14,7 @@ export class PostListByCategoryComponent implements OnInit {
   posts: Array<any>;
   sub: Subscription;
   category: String;
+  categoryId: String;
 
   
   constructor(private postService: PostService, private giphyService: GiphyService, private route: ActivatedRoute) { }
@@ -24,13 +25,14 @@ export class PostListByCategoryComponent implements OnInit {
       console.log("id ", id);
       if (id) {
         this.postService.getAllByCategory(id).subscribe(response => {
-          console.log("response", response);
+          // console.log("response", response);
           this.posts = response ;
           this.category = response[0].category.name;
-          console.log(this.category);
+          this.categoryId = response[0].category.id;
+          // console.log(this.category);
           for (const post of this.posts) {
             if( post.tag === null) {
-            console.log("tag null");
+            // console.log("tag null");
             } else {
               this.giphyService.get(post.tag).subscribe(url => post.giphyUrl = url);
             }
